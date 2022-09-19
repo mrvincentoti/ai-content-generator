686a04dd1161c7661ae4284d72580a4d93c26da8
codewith-zach <92583144+codewith-zach@users.noreply.github.com>
2022-09-19 12:14:34 +0100

    changed logo and created migrations

--- skoloapp/ftthapp/models.py
+++ skoloapp/ftthapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Home(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField()
+
+
+class About(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField()
+
+
+class Pricing(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.IntegerField()
+    duration = models.IntegerField()
+    quantity = models.IntegerField()
+    speed = models.CharField(max_length=255)
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    content = models.TextField()
+
+
+class Faqs(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
