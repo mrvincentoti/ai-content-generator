5db06e3d912c0b9aee08d2cb05f0c8eaf7304471
Georgedroid <wisgeorge.wg@gail.com>
2022-11-28 15:02:57 +0100

    modified ./template/ftth/home.html, and created a custom.js file

---
+++ skoloapp/static/landing/js/custom.js
@@ -0,0 +1,20 @@
+/*function checkAvailability() {
+    console.log('thosbniu');
+}
+*/
+
+(function ($) {
+    "use strict";
+    $("#check").click(function(){
+        alert("The paragraph was clicked");
+      });
+})
+
+
+
+/*$(document).ready(function(){
+    $("#check").click(function(){
+        alert("The button was clicked");
+    });
+  });
+  */
